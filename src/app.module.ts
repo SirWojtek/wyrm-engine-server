addc39bba41fd7f061407a1b9a3508db467872b3
Mateusz Okulewicz <mat@citrusad.com>
2020-04-21 19:53:25 +0200

    encounter controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EncounterModule } from './encounter/encounter.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), EncounterModule],
   controllers: [AppController],
   providers: [AppService],
 })
